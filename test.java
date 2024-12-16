54a3d8256efe1e23720010a560ba60fd4e53148f
1U23CA007AKSHAYKRISHNAN <139437417+1U23CA007AKSHAYKRISHNAN@users.noreply.github.com>
2024-12-16 04:18:06 +0000

    submit

--- test.java
+++ test.java
@@ -1,4 +1,4 @@
-DSA TEST PROGRAM
+
 
 import java.util.Scanner;
 
@@ -9,9 +9,7 @@ class test {
     System.out.println("Enter name, age and salary:");
 
     String name = myObj.nextLine();
-    // 
-       TYPE YOUR CODE HERE
-      //
+    
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Salary: " + salary);
